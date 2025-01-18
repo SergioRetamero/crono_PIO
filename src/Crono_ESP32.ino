44ff58b3a97633b9e09e069b21dac1f7ad5a15a2
SergioRetamero <sergioretamero@hotmail.com>
2025-01-18 12:58:57 +0100

    Primera prueba con esp-now funcionando

--- src/Crono_ESP32.ino
+++ src/Crono_ESP32.ino
@@ -25,9 +25,23 @@
 // Definir el tipo de hardware: FC16-HW
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 
-// Crear los objetos MD_Parola y MD_MAX72XX
-MD_Parola mx = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, NUM_MATRICES);
-MD_MAX72XX mxControl = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, NUM_MATRICES);
+//////////////////////////////////////////
+// Variables de comunicación ESP-NOW
+const uint8_t NODO = 2; // Número de nodo = = receptor, 1 a 10 = transmisores
+
+typedef struct{
+  uint8_t id;
+  uint8_t deciseconds;
+  uint8_t seconds;
+  uint8_t minutes;
+} datoTiempo;
+
+datoTiempo datosRecibidos[10]; // Aquí se guardan los datos recibidos
+// Interface con el receptor
+esp_now_peer_info_t peerInfo;
+
+// Cambiar con la dirección MAC del receptor
+uint8_t broadcastAddress[] = {0x3c, 0x84, 0x27, 0xf2, 0x7d, 0xcc};
 
 // Variables del cronómetro
 volatile int deciseconds = 0;
@@ -35,6 +49,10 @@ volatile int seconds = 0;
 volatile int minutes = 0;
 volatile bool running = false;
 volatile bool countdownFinished = true;
+
+// Crear los objetos MD_Parola y MD_MAX72XX
+MD_Parola mx = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, NUM_MATRICES);
+MD_MAX72XX mxControl = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, NUM_MATRICES);
 
 // Crear objetos Bounce para el debounce de los botones
 Bounce startButton = Bounce();
@@ -64,6 +82,11 @@ void resetTimer();
 void startCountdown();
 void stopTimer();
 void displayTime();
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len);
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+bool registraReceptor();
+bool registraEmisor();
+void nombreDispositivo(char *nombre);
 
 void setup() {
   // Configuración de pines
@@ -88,6 +111,31 @@ void setup() {
   mx.setIntensity(8);
   mxControl.clear();
 
+  // Configurar Ticker
+  ticker.attach_ms(10, incrementTime);
+
+  // Inicializar puerto serie
+  Serial.begin(115200);
+
+  // Inicializar ESP-NOW
+  // Configurar el modo de Wi-Fi
+  WiFi.mode(WIFI_STA);
+  // Inicializar ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error al inicializar ESP-NOW");
+    return;
+  }
+  // Registrar la función de callback
+  if(NODO == 0)
+    registraEmisor();
+  else
+    registraReceptor();
+
+  // Inicializar estructuras de datos
+  reseteaDatosReceptor();
+
+  Serial.println("Sistema iniciado correctamente.");
+
   // Mensaje inicial
   //mx.displayText("CRONO_01", PA_CENTER, 40, 500, PA_SCROLL_LEFT);
   //while (!mx.displayAnimate()) {}
@@ -99,25 +147,37 @@ void setup() {
 
   mx.displayText("AGUSTINOS GR", PA_CENTER, 40, 1200, PA_SCROLL_LEFT);
   while (!mx.displayAnimate()) {}
-  mxControl.clear();
+  mx.displayClear();
+  //mxControl.clear();
 
   digitalWrite(LED_START_PIN, LOW);
   digitalWrite(LED_STOP_PIN, HIGH);
 
   // Mostrar el número 3 en espera
-  drawDigit(3, 14);
-
-  // Configurar Ticker
-  ticker.attach_ms(10, incrementTime);
-
-  // Inicializar puerto serie
-  Serial.begin(115200);
-  Serial.println("Sistema iniciado correctamente.");
+  //drawDigit(3, 14);
+  // Muestra el nombre del dispositivo
+  char nombre[20];
+  nombreDispositivo(nombre);
+  mx.displayText(nombre, PA_CENTER, 40, 1200, PA_SCROLL_UP);
+  while (!mx.displayAnimate()) {}
 }
 
+/////////////////////////////////////////
 void loop() {
   startButton.update();
   stopButton.update();
+
+  // Prueba de envío cada 1 segundo
+  //static uint32_t lastSeconds = 0;
+  static uint32_t lastTime = 0;
+  uint32_t now = millis();
+  //if (seconds != lastSeconds && NODO > 0) { // Actualizar cada 1 segundo
+  //  lastSeconds = seconds;
+  if(now - lastTime >= 5000 && NODO > 0) {
+      lastTime = now;
+      enviarDatosTiempo();
+  }
+
 /**/
   // Manejo de botones
   if (startButton.fell() && !running && countdownFinished) {
@@ -153,6 +213,7 @@ void resetTimer() {
   seconds = 0;
   minutes = 0;
   mxControl.clear();
+  reseteaDatosReceptor();
 }
 
 
@@ -167,7 +228,7 @@ void startCountdown() {
 
   mxControl.clear();
   drawDigit(3, 14);
-
+  Serial.println("Inicio cuenta atrás");
 }
 
 
@@ -202,7 +263,7 @@ void finishCountdown() {
   countdownFinished = true;
   mxControl.clear();
 
-  Serial.println("Crono * 01 * iniciado.");
+  Serial.printf("Crono * %d * iniciado.\n", NODO);
   running = true;
   // Se debe encender el LED de inicio y apagar el de parada
   digitalWrite(LED_START_PIN, LOW);
@@ -216,14 +277,20 @@ void stopTimer() {
   countdownFinished = true;
   digitalWrite(LED_START_PIN, LOW);
   digitalWrite(LED_STOP_PIN, HIGH);
-  Serial.println("Crono * 01 * detenido.");
- // Enviar los datos del cronómetro al puerto serie
-      Serial.print("Tiempo: ");
-      Serial.print(minutes);
-      Serial.print(":");
-      Serial.print(seconds);
-      Serial.print(":");
-      Serial.println(deciseconds);
+
+  if(NODO > 0) {
+    // Enviar los datos del cronómetro por ESP-NOW
+    enviarDatosTiempo(); 
+
+    // Enviar los datos del cronómetro al puerto serie
+    Serial.printf("Crono * %d * detenido.\n", NODO);
+    Serial.print("Tiempo: ");
+    Serial.print(minutes);
+    Serial.print(":");
+    Serial.print(seconds);
+    Serial.print(":");
+    Serial.println(deciseconds);
+  }
 }
 
 void incrementTime() {
@@ -263,4 +330,101 @@ void drawDigit(int digit, int position) {
   for (int i = 0; i < 5; i++) {
     mxControl.setColumn(position + 4 - i, digits[digit][i]);
   }
+}
+
+///////////////////////////////////////////////
+// ESP-NOW
+// funcion que se ejecutara cuando se reciba un paquete
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.printf("Paquete recibido: %s tamaño %d\n", macStr, len);
+  if(len == sizeof(datoTiempo)) {
+    datoTiempo myData;
+    memcpy(&myData, incomingData, sizeof(myData));
+    Serial.printf("ID de la placa %u\t", myData.id);
+    // Actualizar la estructura con los nuevos datos recibidos
+    datosRecibidos[myData.id-1].deciseconds = myData.deciseconds;
+    datosRecibidos[myData.id-1].seconds = myData.seconds;
+    datosRecibidos[myData.id-1].minutes = myData.minutes;
+    Serial.printf("Tiempo: %02d:%02d.%02d\n", 
+      datosRecibidos[myData.id-1].minutes, 
+      datosRecibidos[myData.id-1].seconds, 
+      datosRecibidos[myData.id-1].deciseconds);
+  }
+}
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("Estado ultimo paquete:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Envio Correcto" : "Envio fallido");
+}
+
+// Pone todos los datos recibidos a 0
+void reseteaDatosReceptor() {
+  for (int i = 0; i < 10; i++) {
+    datosRecibidos[i].id = 0;
+    datosRecibidos[i].deciseconds = 0;
+    datosRecibidos[i].seconds = 0;
+    datosRecibidos[i].minutes = 0;
+  }
+}
+
+bool registraReceptor(){
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  // Redistra la función de callback de envío
+  esp_now_register_send_cb(OnDataSent);
+  
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return false;
+  } else {
+    Serial.println("Receptor registrado con éxito");
+  }
+  return true;
+}
+
+bool registraEmisor(){
+  // Registrar la función de callback de recepción
+  esp_err_t result = esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+  if (result == ESP_OK) {
+    Serial.println("Recepción registrada con éxito");
+    return true;
+  }
+  else {
+    Serial.println("Error al registrar para envío");
+    return false;
+  }
+}
+
+bool enviarDatosTiempo() {
+  datoTiempo myData;
+  myData.id = NODO;
+  myData.deciseconds = deciseconds;
+  myData.seconds = seconds;
+  myData.minutes = minutes;
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+  if (result == ESP_OK) {
+    //Serial.println("Enviado con éxito");
+    return true;
+  }
+  else {
+    Serial.println("Error al enviar los datos");
+    return false;
+  }
+}
+
+
+void nombreDispositivo(char *nombre){
+  if(NODO == 0)
+    sprintf(nombre, "Centro");
+  else
+    sprintf(nombre, "Nodo %d", NODO);
 }
